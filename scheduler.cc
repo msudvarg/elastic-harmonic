f92a576b3e111cd6b4dd80f62d1cea8293ed244d
Marion Sudvarg <msudvarg@gmail.com>
2023-10-13 14:22:47 -0500

    Task constructor is easier now

--- scheduler.cc
+++ scheduler.cc
@@ -9,9 +9,10 @@ int main(int argc, char * argv[]) {
     float u = 1;
     if (argc > 1) u = atof(argv[1]);
 
-    elastic_space.add_task(Task {5,6,1,0.3,3});
-    elastic_space.add_task(Task {12,17,1,0.7,4});
-    elastic_space.add_task(Task {23,36,1,0.1,1});
+    //Task takes T_min, T_max, C, E
+    elastic_space.add_task(Task {5,6,0.3,3});
+    elastic_space.add_task(Task {12,17,0.7,4});
+    elastic_space.add_task(Task {23,36,0.1,1});
     elastic_space.generate();
 
     if(!elastic_space.assign_periods_slow(u)) {
--- task.h
+++ task.h
@@ -20,6 +20,11 @@ struct Task {
     inline float u() const {
         return c/(float)t;
     }
+
+    Task(float t_min, float t_max, float _c, float _e) :
+        i {t_min, t_max},
+        c {_c},
+        e {_e} {}
 };
 
 using Tasks = std::vector<Task>;
