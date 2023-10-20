fa11e764d64e145c74e37f1c48bdba5ff8234fc6
Marion Sudvarg <msudvarg@gmail.com>
2023-10-19 20:23:40 -0500

    More tests in scheduler

--- scheduler.cc
+++ scheduler.cc
@@ -15,7 +15,16 @@ int main(int argc, char * argv[]) {
     elastic_space.add_task(Task {23,36,0.1,1});
     elastic_space.generate();
 
-    for (float u = 0.1; u < 0.12; u+=0.001) {
+    std::cout << "\nRegion Space:\n";
+    for (const Region & region : elastic_space.regions) {
+        std::cout << region.lb << ' ' << region.ub << ' ' << (region.chain - elastic_space.chains.data()) << std::endl;
+    }
+    std::cout << "\nChains:\n";
+    for (const Chain & chain : elastic_space.chains) {
+        std::cout << chain.u_min << ' ' << chain.u_max << std::endl;
+    }
+
+    for (float u = 0.095; u < 0.15; u+=0.001) {
 
         Chain * chain1 = elastic_space.assign_periods_slow(u);
         if(!chain1) {
@@ -26,6 +35,10 @@ int main(int argc, char * argv[]) {
         print_info(elastic_space.tasks);
 
         Chain * chain2 = elastic_space.assign_periods(u);
+        if(!chain2) {
+            std::cout << "Utilization " << u << " is too low!" << std::endl;
+            continue;
+        }
         std::cout << "\n";
         print_info(elastic_space.tasks);
         
