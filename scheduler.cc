9c463943b32a51fe6ab2dd0ba07a09f954d296d8
Marion Sudvarg <msudvarg@gmail.com>
2023-10-21 13:18:36 -0500

    Wrapped some output into DEBUGINFO flag

--- scheduler.cc
+++ scheduler.cc
@@ -15,6 +15,7 @@ int main(int argc, char * argv[]) {
     elastic_space.add_task(Task {23,36,0.1,1});
     elastic_space.generate();
 
+#ifdef DEBUGINFO
     std::cout << "\nRegion Space:\n";
     for (const Region & region : elastic_space.regions) {
         std::cout << region.lb << ' ' << region.ub << ' ' << (region.chain - elastic_space.chains.data()) << std::endl;
@@ -23,8 +24,11 @@ int main(int argc, char * argv[]) {
     for (const Chain & chain : elastic_space.chains) {
         std::cout << chain.u_min << ' ' << chain.u_max << std::endl;
     }
+#endif
 
-    for (float u = 0.095; u < 0.15; u+=0.001) {
+    std::cout << "Minimum utilization accommodated: " << elastic_space.u_min << std::endl;
+
+    for (float u = 0.1; u < 0.6; u+=0.1) {
 
         Chain * chain1 = elastic_space.assign_periods_slow(u);
         if(!chain1) {
@@ -34,6 +38,7 @@ int main(int argc, char * argv[]) {
         std::cout << "\n";
         print_info(elastic_space.tasks);
 
+#ifdef DEBUGINFO
         Chain * chain2 = elastic_space.assign_periods(u);
         if(!chain2) {
             std::cout << "Utilization " << u << " is too low!" << std::endl;
@@ -44,6 +49,12 @@ int main(int argc, char * argv[]) {
         
         std::cout << "\nUtilization " << u << " chains " << elastic_space.chains.size() << ' '
                   << (chain1 - elastic_space.chains.data()) << ' ' << (chain2 - elastic_space.chains.data()) << std::endl;
+
+        if (chain1 != chain2) {
+            std::cout << "Problem! Chains selected by slow and fast methods to dnot match!" << std::endl;
+            return -1;
+        }
+#endif
 
 
     }
