6c0517c892206ddfa3bdb9477096022c98d9d7fb
Marion Sudvarg <msudvarg@gmail.com>
2024-01-27 14:00:16 -0600

    Better documentation in enumerate_phi.cc

--- enumerate_phi/enumerate_phi.cc
+++ enumerate_phi/enumerate_phi.cc
@@ -28,10 +28,14 @@ long long counts[max_tasks][k];
 /*
     recurse_ind(const int prod, const int task, const int n_tasks)
 
-    Given a system of `n_tasks`
+    Recursively project harmonic intervals.
 
-    Recursively compute number of projected harmonic intervals
-    from task at index `task`
+    We have a system of `n_tasks`
+    At task number `task`-1 we have already achieved a product of `prod`
+    In other words,
+    a_2 * a_3 * ... * a_{task-1} = prod
+
+    We can now project onto a_task and further to the end
 
 */
 void recurse_ind(const int prod, const int task, const int n_tasks) {
@@ -40,12 +44,23 @@ void recurse_ind(const int prod, const int task, const int n_tasks) {
     if(prod > k) return;
 
     for (int i = 1; i <= k; ++i) {
+        //prod = a_2 * a_3 * ... * a_{task-1}
+        //x = prod * i, where i represents a_task
         const int x = i * prod;
+
+        //We are at the last task
         if (task == n_tasks) {
+
+            //As long as the total product x does not exceed k,
+            //Increase the count of ways to achieve a multiple equal to x
             if (x <= k) counts[task-1][x-1]++;
             else return;
         }
+
+        //We are not at the last task
         else {
+            
+            //Continue to the end
             recurse_ind(x, task+1, n_tasks);
         }
     }
@@ -56,6 +71,17 @@ void recurse_ind(const int prod, const int task, const int n_tasks) {
 
     Computes the number of projected harmonic intervals for values of n and k.
     Assumes all tasks have a minimum period of 1 to maximize the number of intervals.
+
+    We call h*_{n,l} the number of ways to generate a sequence of positive integer multiples:
+    a_2 * a_3 * ... * a_n = l
+
+    Then the number of projected harmonic intervals h_{n,k} is
+    sum_{l=1}^k h*_{n,l}
+
+    For all values of n and l from 1--k, we recursively compute h*_{n,l}
+
+    Then we compute h_{n,k} by summing over the h* values.
+
 */
 void get_harmonic_counts() {
 
@@ -65,7 +91,12 @@ void get_harmonic_counts() {
         int begin = omp_get_thread_num();
         int step = omp_get_num_threads();
         for (int n_tasks = begin; n_tasks < max_tasks; n_tasks += step) {
+
+            //Recursively compute all values h*_{n,l} for a given n
+            //(We use n_tasks+1 due to 0-indexing)
             recurse_ind(1, 1, n_tasks+1);
+
+            //Compute h_{n,k} by summing over h* values
             for (int i = 1; i < k; ++i) {
                 counts[n_tasks][i] += counts[n_tasks][i-1];
             }
